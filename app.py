41cbd1c1a94ff100c610e0e6055d060a16603bd9
Kevin Cruz <kocruz.24@gmail.com>
2021-07-22 20:10:56 -0400

    Reformatting code

--- app.py
+++ app.py
@@ -150,11 +150,13 @@ def show_venue(venue_id):
         "facebook_link": "https://www.facebook.com/TheMusicalHop",
         "seeking_talent": True,
         "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
         "past_shows": [{
             "artist_id": 4,
             "artist_name": "Guns N Petals",
-            "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+            "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid"
+                                 "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
             "start_time": "2019-05-21T21:30:00.000Z"
         }],
         "upcoming_shows": [],
@@ -172,7 +174,8 @@ def show_venue(venue_id):
         "website": "https://www.theduelingpianos.com",
         "facebook_link": "https://www.facebook.com/theduelingpianos",
         "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         "past_shows": [],
         "upcoming_shows": [],
         "past_shows_count": 0,
@@ -189,27 +192,32 @@ def show_venue(venue_id):
         "website": "https://www.parksquarelivemusicandcoffee.com",
         "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
         "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+        "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
         "past_shows": [{
             "artist_id": 5,
             "artist_name": "Matt Quevedo",
-            "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+            "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid"
+                                 "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
             "start_time": "2019-06-15T23:00:00.000Z"
         }],
         "upcoming_shows": [{
             "artist_id": 6,
             "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                                 "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
             "start_time": "2035-04-01T20:00:00.000Z"
         }, {
             "artist_id": 6,
             "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                                 "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
             "start_time": "2035-04-08T20:00:00.000Z"
         }, {
             "artist_id": 6,
             "artist_name": "The Wild Sax Band",
-            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+            "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                                 "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
             "start_time": "2035-04-15T20:00:00.000Z"
         }],
         "past_shows_count": 1,
@@ -301,11 +309,13 @@ def show_artist(artist_id):
         "facebook_link": "https://www.facebook.com/GunsNPetals",
         "seeking_venue": True,
         "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
         "past_shows": [{
             "venue_id": 1,
             "venue_name": "The Musical Hop",
-            "venue_image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+            "venue_image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid"
+                                "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
             "start_time": "2019-05-21T21:30:00.000Z"
         }],
         "upcoming_shows": [],
@@ -321,11 +331,13 @@ def show_artist(artist_id):
         "phone": "300-400-5000",
         "facebook_link": "https://www.facebook.com/mattquevedo923251523",
         "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+        "image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
         "past_shows": [{
             "venue_id": 3,
             "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid"
+                                "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
             "start_time": "2019-06-15T23:00:00.000Z"
         }],
         "upcoming_shows": [],
@@ -340,22 +352,26 @@ def show_artist(artist_id):
         "state": "CA",
         "phone": "432-325-5432",
         "seeking_venue": False,
-        "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+        "image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
         "past_shows": [],
         "upcoming_shows": [{
             "venue_id": 3,
             "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid"
+                                "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
             "start_time": "2035-04-01T20:00:00.000Z"
         }, {
             "venue_id": 3,
             "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid"
+                                "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
             "start_time": "2035-04-08T20:00:00.000Z"
         }, {
             "venue_id": 3,
             "venue_name": "Park Square Live Music & Coffee",
-            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
+            "venue_image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid"
+                                "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
             "start_time": "2035-04-15T20:00:00.000Z"
         }],
         "past_shows_count": 0,
@@ -381,7 +397,8 @@ def edit_artist(artist_id):
         "facebook_link": "https://www.facebook.com/GunsNPetals",
         "seeking_venue": True,
         "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
+        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80 "
     }
     # TODO: populate form with fields from artist with ID <artist_id>
     return render_template('forms/edit_artist.html', form=form, artist=artist)
@@ -410,7 +427,8 @@ def edit_venue(venue_id):
         "facebook_link": "https://www.facebook.com/TheMusicalHop",
         "seeking_talent": True,
         "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid"
+                      "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60 "
     }
     # TODO: populate form with values from venue with ID <venue_id>
     return render_template('forms/edit_venue.html', form=form, venue=venue)
@@ -458,35 +476,40 @@ def shows():
         "venue_name": "The Musical Hop",
         "artist_id": 4,
         "artist_name": "Guns N Petals",
-        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
+        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid"
+                             "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
         "start_time": "2019-05-21T21:30:00.000Z"
     }, {
         "venue_id": 3,
         "venue_name": "Park Square Live Music & Coffee",
         "artist_id": 5,
         "artist_name": "Matt Quevedo",
-        "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+        "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid"
+                             "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
         "start_time": "2019-06-15T23:00:00.000Z"
     }, {
         "venue_id": 3,
         "venue_name": "Park Square Live Music & Coffee",
         "artist_id": 6,
         "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                             "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
         "start_time": "2035-04-01T20:00:00.000Z"
     }, {
         "venue_id": 3,
         "venue_name": "Park Square Live Music & Coffee",
         "artist_id": 6,
         "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                             "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
         "start_time": "2035-04-08T20:00:00.000Z"
     }, {
         "venue_id": 3,
         "venue_name": "Park Square Live Music & Coffee",
         "artist_id": 6,
         "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
+        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid"
+                             "=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
         "start_time": "2035-04-15T20:00:00.000Z"
     }]
     return render_template('pages/shows.html', shows=data)
