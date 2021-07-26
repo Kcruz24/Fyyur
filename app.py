8e6e665bd9ef31fde9ab40ea875ed401adec0e74
Kevin Cruz <kocruz.24@gmail.com>
2021-07-26 04:44:58 -0400

    Display Artists from the database

--- app.py
+++ app.py
@@ -368,17 +368,19 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-    # TODO: replace with real data returned from querying the database
-    data = [{
-        "id": 4,
-        "name": "Guns N Petals",
-    }, {
-        "id": 5,
-        "name": "Matt Quevedo",
-    }, {
-        "id": 6,
-        "name": "The Wild Sax Band",
-    }]
+    # TODO: replace with real data returned from querying the database (DONE)
+    data = Artist.query.all()
+
+    # data = [{
+    #     "id": 4,
+    #     "name": "Guns N Petals",
+    # }, {
+    #     "id": 5,
+    #     "name": "Matt Quevedo",
+    # }, {
+    #     "id": 6,
+    #     "name": "The Wild Sax Band",
+    # }]
     return render_template('pages/artists.html', artists=data)
 
 
