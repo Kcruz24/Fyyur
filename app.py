fb5e56e4e078cab8ac7b2eacfa1f71c371050d38
Kevin Cruz <kocruz.24@gmail.com>
2021-07-28 18:48:50 -0400

    Implement search_artist() post route successfully

--- app.py
+++ app.py
@@ -398,19 +398,24 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive. (DONE)
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
-    return render_template('pages/search_artists.html', results=response,
-                           search_term=request.form.get('search_term', ''))
+
+    search_term = request.form.get('search_term')
+    all_artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).all()
+    count = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).count()
+
+    # response = {
+    #     "count": 1,
+    #     "data": [{
+    #         "id": 4,
+    #         "name": "Guns N Petals",
+    #         "num_upcoming_shows": 0,
+    #     }]
+    # }
+    return render_template('pages/search_artists.html', artists=all_artists, count=count,
+                           search_term=search_term)
 
 
 @app.route('/artists/<int:artist_id>')
