e4cf234d26f67e8b96e951a7519d48893f78204e
Kevin Cruz <kocruz.24@gmail.com>
2021-07-30 03:08:09 -0400

    Display artists names on artists.html properly - backend

--- app.py
+++ app.py
@@ -341,18 +341,15 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
     # TODO: replace with real data returned from querying the database (DONE)
-    data = Artist.query.all()
+    get_artists = Artist.query.all()
+    data = []
 
-    # data = [{
-    #     "id": 4,
-    #     "name": "Guns N Petals",
-    # }, {
-    #     "id": 5,
-    #     "name": "Matt Quevedo",
-    # }, {
-    #     "id": 6,
-    #     "name": "The Wild Sax Band",
-    # }]
+    for artist in get_artists:
+        data.append({
+            "id": artist.id,
+            "name": artist.name
+        })
+
     return render_template('pages/artists.html', artists=data)
 
 
