01a6521e80ffcd64c0bb629713cd9f9c084a2ae8
Kevin Cruz <kocruz.24@gmail.com>
2021-08-01 20:04:33 -0400

    Delete a comma on format_datetime()

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ from flask import render_template, request, flash, redirect, url_for, abort
 def format_datetime(value, format='medium'):
     date = dateutil.parser.parse(value)
     if format == 'full':
-        format = "EEEE MMMM, d, y 'at' h:mma"
+        format = "EEEE MMMM d, y 'at' h:mma"
     elif format == 'medium':
-        format = "EE MM, dd, y h:mma"
+        format = "EE MM dd, y h:mma"
     return babel.dates.format_datetime(date, format, locale='en')
 
 
