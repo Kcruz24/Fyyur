851391bc0759e7f33e7f8c83d17502d2baaac277
Kevin Cruz <kocruz.24@gmail.com>
2021-08-01 22:31:23 -0400

    Remove app from models.py

--- models.py
+++ models.py
@@ -3,11 +3,12 @@ from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String
+from app import app
 
 # ----------------------------------------------------------------------------#
 # App Config.
 # ----------------------------------------------------------------------------#
-app = Flask(__name__)
+
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
